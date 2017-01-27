0f38db10e98581ef2bfaf76b5f43100ad2911bad
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-27 19:15:48 +0100

    improved images handling for list of images and featuring on homepage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
 	belongs_to :productrange
+
+	def image
+		src = self.imgs.split("\r\n").select{ |x| x.match('.jpg' || '.png' || '.jpeg' ) }.first
+		src || "http://placekitten.com/500/400"
+	end
 end
