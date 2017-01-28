dddeb1483fb8fda0bb9301c64557a9ae6395536e
BrunoBoehm <bruno.boehm@gmail.com>
2017-01-28 14:13:01 +0100

    refactored into helpers

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,27 @@
 module ProductsHelper
+	def image(product)
+		src = product.imgs.split("\r\n").select{ |x| x.match('.jpg' || '.png' || '.jpeg' ) }.first
+		src || "http://placekitten.com/500/400"
+	end
+
+	def images(product)
+		product.imgs.split("\r\n").select{ |x| x.match('.jpg' || '.png' || '.jpeg' ) }
+	end
+
+	def completion(product)
+		completed = product.attributes.values.select(&:blank?).count
+		total = product.attributes.count - 2
+		completion = 100 - completed * 100 / total
+	end
+
+	def completion_state(product)
+		case 
+			when completion(product) == 100
+				"success"
+			when completion(product) < 100 && completion(product) >= 50
+				"warning"
+			when completion(product) < 50
+				"danger"
+		end		
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,3 @@
 class Product < ActiveRecord::Base
 	belongs_to :productrange
-
-	def image
-		src = self.imgs.split("\r\n").select{ |x| x.match('.jpg' || '.png' || '.jpeg' ) }.first
-		src || "http://placekitten.com/500/400"
-	end
 end
